b2bb5c2269e6cfba412216a6823d79655572babb
komcal <kom.sujautra@gmail.com>
2017-04-24 22:48:16 +0700

    simple query

--- src/routes/sample.js
+++ src/routes/sample.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { respondResult, respondErrors } from '../utilities';
+import pool from '../db-config';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send('Ok');
+});
+
+router.get('/query', async (req, res) => {
+  try {
+    const response = await pool.query(`
+      SELECT * FROM FARM;
+    `);
+    respondResult(res)(response.rows);
+  } catch (err) {
+    respondErrors(res)(err);
+  }
 });
 
 export default router;
