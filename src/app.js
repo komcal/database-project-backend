f784b2f4dec8e3b9026d168a6b6ba3b864964e20
komcal <kom.sujautra@gmail.com>
2017-04-24 22:25:41 +0700

    add more app.use

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import routes from './routes';
 
 const express = require('express');
+const lusca = require('lusca');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+app.disable('etag');
+app.use(cors());
 app.use('/', routes);
 
 export default app;
