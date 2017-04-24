46498e45bfdc302c9a8df1faffc4d5b5ce535c30
komcal <kom.sujautra@gmail.com>
2017-04-24 22:22:40 +0700

    create sample route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+import routes from './routes';
+
 const express = require('express');
 
 const app = express();
+
+app.use('/', routes);
 
 export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import sample from './sample';
+
+const router = Router();
+router.use('/sample', sample);
+
+export default router;
